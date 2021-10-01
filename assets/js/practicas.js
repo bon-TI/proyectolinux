1190f30dc1d5acd278670453cdb7ca49b1d8294f
bon <joseipaz1203@gmail.com>
2021-10-01 12:10:02 -0700

    Mi primer commit

---
+++ assets/js/practicas.js
@@ -0,0 +1,407 @@
+/* PDF VIEWER 1*/  
+function mostrarPdf1() {
+  element1 = document.getElementById('pdfviewer1');
+  estado1 = element1.style.display;
+  if (estado1 == 'none') {
+    element1.style.display = 'inline-block';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  } else {
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  }
+
+}
+
+/* PDF VIEWER 2*/  
+function mostrarPdf2() {
+  element2 = document.getElementById('pdfviewer2');
+  estado2 = element2.style.display;
+  if (estado2 == 'none') {
+    element2.style.display = 'inline-block';
+    element1.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  } else {
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  }
+
+}
+
+/* PDF VIEWER 3*/  
+function mostrarPdf3() {
+  element3 = document.getElementById('pdfviewer3');
+  estado3 = element3.style.display;
+  if (estado3 == 'none') {
+    element3.style.display = 'inline-block';
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  } else {
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  }
+
+}
+
+/* PDF VIEWER 4*/  
+function mostrarPdf4() {
+  element4 = document.getElementById('pdfviewer4');
+  estado4 = element4.style.display;
+  if (estado4 == 'none') {
+    element4.style.display = 'inline-block';
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  } else {
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  }
+
+}
+
+/* PDF VIEWER 5*/  
+function mostrarPdf5() {
+  element5 = document.getElementById('pdfviewer5');
+  estado5 = element5.style.display;
+  if (estado5 == 'none') {
+    element5.style.display = 'inline-block';
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  } else {
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  }
+
+}
+
+/* PDF VIEWER 6*/  
+function mostrarPdf6() {
+  element6 = document.getElementById('pdfviewer6');
+  estado6 = element6.style.display;
+  if (estado6 == 'none') {
+    element6.style.display = 'inline-block';
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  } else {
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  }
+
+}
+
+/* PDF VIEWER 7*/  
+function mostrarPdf7() {
+  element7 = document.getElementById('pdfviewer7');
+  estado7 = element7.style.display;
+  if (estado7 == 'none') {
+    element7.style.display = 'inline-block';
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  } else {
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  }
+
+}
+
+/* PDF VIEWER 8*/  
+function mostrarPdf8() {
+  element8 = document.getElementById('pdfviewer8');
+  estado8 = element8.style.display;
+  if (estado8 == 'none') {
+    element8.style.display = 'inline-block';
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  } else {
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  }
+
+}
+
+/* PDF VIEWER 9*/  
+function mostrarPdf9() {
+  element9 = document.getElementById('pdfviewer9');
+  estado9 = element9.style.display;
+  if (estado9 == 'none') {
+    element9.style.display = 'inline-block';
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  } else {
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  }
+
+}
+
+/* PDF VIEWER 10*/  
+function mostrarPdf10() {
+  element10 = document.getElementById('pdfviewer10');
+  estado10 = element10.style.display;
+  if (estado10 == 'none') {
+    element10.style.display = 'inline-block';
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  } else {
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  }
+
+}
+
+/* PDF VIEWER 11*/  
+function mostrarPdf11() {
+  element11 = document.getElementById('pdfviewer11');
+  estado11 = element11.style.display;
+  if (estado11 == 'none') {
+    element11.style.display = 'inline-block';
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element12.style.display = 'none';
+  } else {
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  }
+
+}
+
+/* PDF VIEWER 11*/  
+function mostrarPdf12() {
+  element12 = document.getElementById('pdfviewer12');
+  estado12 = element12.style.display;
+  if (estado12 == 'none') {
+    element12.style.display = 'inline-block';
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+  } else {
+    element1.style.display = 'none';
+    element2.style.display = 'none';
+    element3.style.display = 'none';
+    element4.style.display = 'none';
+    element5.style.display = 'none';
+    element6.style.display = 'none';
+    element7.style.display = 'none';
+    element8.style.display = 'none';
+    element9.style.display = 'none';
+    element10.style.display = 'none';
+    element11.style.display = 'none';
+    element12.style.display = 'none';
+  }
+
+}
